20385268d60cd2ce697ed332f7f2bb5a0c2a253c
Richard Hess <eswat2@gmail.com>
2020-01-30 18:37:52 -0800

    complete set of ikons

---
+++ src/mitsubishi.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let hex = undefined
+  export let size = 24
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 80.4 80.4"
+  width={size}
+  height={size}>
+  <g fill={hex}>
+    <path
+      d="M39.7 7.2L27 27.6l25.5 44.5 26.8-.1-13.6-22H14.3L1.1 72.3h25l26.8-45.1z" />
+  </g>
+</svg>

be6d853ff24c9834557c926fc77700a970b540e0
Richard Hess <eswat2@gmail.com>
2020-01-30 17:46:00 -0800

    another batch

---
+++ src/dodge.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let hex = undefined
+  export let size = 24
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 100.4 100.4"
+  width={size}
+  height={size}>
+  <g fill={hex}>
+    <path
+      d="M0 76.8l46.3-53.7h22.5L22.5 76.8H0zm31.6 0l46.3-53.7h22.5L54.1
+      76.8H31.6z" />
+  </g>
+</svg>
